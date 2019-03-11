4e753891f90d49aa9ab38d4cc4fcb7307c684b00
Henrique Simoes <henriquesimoespa@gmail.com>
2019-03-11 11:22:15 -0300

    Add basic features

--- bot.js
+++ bot.js
@@ -2,8 +2,15 @@
 // Licensed under the MIT License.
 
 const { ActivityTypes } = require('botbuilder');
+const request = require('request');
+const {JSDOM} = require('jsdom');
+
 
 class MyBot {
+    
+    constructor () {
+        this.greeting = false;
+    }
     /**
      *
      * @param {TurnContext} on turn context object.
@@ -11,9 +18,28 @@ class MyBot {
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
-        } else {
-            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
+            const message = turnContext.activity.text.toUpperCase();
+            const subject = await this.loadSubject(message);
+            if (subject !== null) {
+                await turnContext.sendActivity(`${subject.name}\n\n${subject.description}`);
+            } else {
+                await turnContext.sendActivity(`Disciplina não encontrada. :(`);
+            }
+        }
+    }
+
+    async loadSubject(subjectName, year = 2019) {
+        const dom = await JSDOM.fromURL(`https://www.dac.unicamp.br/sistemas/catalogos/grad/catalogo${year}/coordenadorias/0023/0023.html`);
+
+        const referenceElement = dom.window.document.querySelector(`a[name='${subjectName}']`);
+        const subjectDiv = referenceElement !== null ? referenceElement.parentElement : null;
+
+        // if subject not found return null
+        if (subjectDiv === null) return null;
+        
+        return {
+            name: subjectDiv.querySelector('a').innerHTML,
+            description: subjectDiv.nextElementSibling.nextElementSibling.querySelector('p').innerHTML
         }
     }
 }
